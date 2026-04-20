548f320e22522c9f572aa60f113f109bdc7bee98
faraz18001 <faraz18001@users.noreply.github.com>
2026-04-20 16:45:31 +0500

    removed useless files

--- explore_pymupdf4llm.py
+++
@@ -1,6 +0,0 @@
-import pymupdf4llm
-
-pdf_path = "data/5054_w25_qp_11.pdf"
-md = pymupdf4llm.to_markdown(pdf_path, header=False, footer=False)
-
-print(md[:1000])
--- save_md.py
+++
@@ -1,7 +0,0 @@
-import pymupdf4llm
-from pathlib import Path
-
-pdf_path = "data/5054_w25_qp_11.pdf"
-md = pymupdf4llm.to_markdown(pdf_path, header=False, footer=False)
-Path("output_md.md").write_text(md)
-print("Markdown saved to output_md.md")
